0c0dd4fa31e20965ea8acc6b408f62d2897a7372
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-13 16:01:06 -0800

    delete second post

--- setup.sh
+++ setup.sh
@@ -108,20 +108,6 @@ categories: ["blog"]
 This is my first post using Hugo with PaperMod theme!
 EOF
 
-# Create second post
-cat >content/posts/my-second-post.md <<EOF
----
-title: "My second Post"
-date: ${CURRENT_DATE}
-draft: second
-summary: "Welcome to my second post!"
-tags: ["second"]
-categories: ["blog"]
----
-
-This is my second post using Hugo with PaperMod theme!
-EOF
-
 # Create search page
 cat >content/search.md <<EOF
 ---
